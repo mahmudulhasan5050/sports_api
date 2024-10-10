c9f37fc7fcf985bdc8b0ac5141defe5248a9101e
mahmudulhasan5050 <50146955+mahmudulhasan5050@users.noreply.github.com>
2024-10-10 18:45:44 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ mongoose
   });
 
   var corsOptions = {
-    origin: ['http://localhost:3000', '*'],
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
